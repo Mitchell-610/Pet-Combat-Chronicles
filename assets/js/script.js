b38141e2b01353028fa6ed0968f2f9c66579b917
Mitchell-610 <Mheit602@gmail.com>
2024-06-20 21:41:51 -0400

    Function added to create card of pop up info.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,33 @@ randomBtn.addEventListener(`click`, function (event) {
     event.preventDefault();
 
 });
+
+
+function createCard() {
+    const popup = $('<div>')
+    .addClass('card task-card my-3')
+    .attr('data-task-id', breed);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.type);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-task-id', task.id);
+    cardDeleteBtn.on('click', handleDeleteTask);
+    if (task.dueDate && task.status !== 'done') {
+      const now = dayjs();
+      const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+      if (now.isSame(taskDueDate, 'day')) {
+        taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+        taskCard.addClass('bg-danger text-white');
+        cardDeleteBtn.addClass('border-light');
+    }
+    }
+    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+    taskCard.append(cardHeader, cardBody);
+    return taskCard;
+  
+  };
