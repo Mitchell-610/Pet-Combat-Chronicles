234dd73f86ef1e767fdd41c278e252d7274a4f8a
Mitchell-610 <Mheit602@gmail.com>
2024-06-20 21:23:54 -0400

    Consts added for name value and breed value in input form. RandomBtn onclick function added but blank.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 const randomBtn = document.querySelector(`#randomizeBtn`);
 const fightBtn = document.querySelector(`#fightBtn`);
 const pastBtn = document.querySelector(`#pastBtn`);
+const nameValue = document.querySelector(`#searchName`);
+const breedValue = document.querySelector(`#searchBreed`);
+
+randomBtn.addEventListener(`click`, function (event) {
+    event.preventDefault();
+
+});
