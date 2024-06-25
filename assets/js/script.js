e9abd0545d457ff0a124d80188e69a4e2db96436
Mitchell-610 <Mheit602@gmail.com>
2024-06-24 20:58:13 -0400

    Added the current JS file within.

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+
+
+const randomBtn = document.querySelector(`#randomizeBtn`);
+const battleBtn = document.querySelector(`#battleBtn`);
+const pastBtn = document.querySelector(`#pastBtn`);
+const nameValue = document.querySelector(`#searchName`);
+const breedValue = document.querySelector(`#searchBreed`);
+
+fetch("https://dogapi.dog/api/v2/breeds")
+    .then(response => response.json())
+    //.then (data => console.log(data))
+    .then (data => console.log(data.data))
+    .catch(error => console.error(error))  
+
+    function rollDice() {
+        // Generate a random number between 1 and 6
+        const diceRoll = Math.floor(Math.random() * 6) + 1;
+        
+        // Display the result in the console
+        console.log("You rolled a " + diceRoll);
+    }
+    
+    // Call the function to roll the dice
+    rollDice();
+
+
+//For popup at end of battle.
+function createCard() {
+    const popup = $('<div>')
+        .addClass('card task-card my-3')
+        .attr('data-task-id', breed);
+    const cardHeader = $('<div>').addClass('card-header h4').text(breed.value);
+    const cardBody = $('<div>').addClass('card-body');
+
+
+    const dogLife = $('<p>').addClass('card-text').text(/* NEED TO CREATE CONST FOR DOGS LIFE POINTS VALUE */  life.value);
+    const dogHypo = $('<p>').addClass('card-text').text(/* CREATE CONST FOR DOGS HYPO ALLERGIES VALUE */);
+    const weight = $('<p>').addClass('card-text').text(/* CREATE CONST FOR WEIGHT VALUE */);
+
+    const close = $('<button>')
+        .addClass('btn btn-danger close')
+        .text('Close')
+        
+
+        //This down needs to be tied into close card function neither of them finished.
+    close.on('click', closeCard);
+    return popup;
+
+};
+
+//To close popup at end of battle.
+function closeCard(event) {
+    event.preventDefault();
+
+    if (close === true) {
+
+    }
+
+};
+
+//Getting random dogs.
+/*randomBtn.addEventListener(`click`, function (event) {
+    event.preventDefault();
+
+});*/
